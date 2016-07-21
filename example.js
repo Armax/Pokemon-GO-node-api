9f618bcc8f9e4f4c354fd8c4bc8f44808eff56c7
Armax <marcocuci@gmail.com>
2016-07-22 01:12:16 +0200

    updated example

--- example.js
+++ example.js
@@ -1,9 +1,18 @@
 'use strict';
 
-var Pokeio = require('./poke.io');
+var PokemonGO = require('./poke.io.js');
+
+// using var so you can login with multiple users
+var a = new PokemonGO.Pokeio()
+var b = new PokemonGO.Pokeio()
 
 //Set environment variables or replace placeholder text
 var location = {
+    type: 'name',
+    name: process.env.PGO_LOCATION || 'Times Square'
+};
+
+var location1 = {
     type: 'name',
     name: process.env.PGO_LOCATION || 'Times Square'
 };
@@ -12,13 +21,58 @@ var username = process.env.PGO_USERNAME || 'USER';
 var password = process.env.PGO_PASSWORD || 'PASS';
 var provider = process.env.PGO_PROVIDER || 'google';
 
-Pokeio.init(username, password, location, provider, function(err) {
+var username1 = process.env.PGO_USERNAME || 'USER';
+var password1 = process.env.PGO_PASSWORD || 'PASS';
+var provider1 = process.env.PGO_PROVIDER || 'google';
+
+a.init(username, password, location, provider, function(err) {
     if (err) throw err;
 
-    console.log('[i] Current location: ' + Pokeio.playerInfo.locationName);
-    console.log('[i] lat/long/alt: : ' + Pokeio.playerInfo.latitude + ' ' + Pokeio.playerInfo.longitude + ' ' + Pokeio.playerInfo.altitude);
+    console.log('1[i] Current location: ' + a.playerInfo.locationName);
+    console.log('1[i] lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
 
-    Pokeio.GetProfile(function(err, profile) {
+    a.GetProfile(function(err, profile) {
+        if (err) throw err;
+
+        console.log('1[i] Username: ' + profile.username);
+        console.log('1[i] Poke Storage: ' + profile.poke_storage);
+        console.log('1[i] Item Storage: ' + profile.item_storage);
+
+        var poke = 0;
+        if (profile.currency[0].amount) {
+            poke = profile.currency[0].amount;
+        }
+
+        console.log('1[i] Pokecoin: ' + poke);
+        console.log('1[i] Stardust: ' + profile.currency[1].amount);
+
+        setInterval(function(){
+            a.Heartbeat(function(err,hb) {
+                if(err) {
+                    console.log(err);
+                }
+
+                for (var i = hb.cells.length - 1; i >= 0; i--) {
+                    if(hb.cells[i].NearbyPokemon[0]) {
+                        //console.log(a.pokemonlist[0])
+                        var pokemon = a.pokemonlist[parseInt(hb.cells[i].NearbyPokemon[0].PokedexNumber)-1]
+                        console.log('1[+] There is a ' + pokemon.name + ' at ' + hb.cells[i].NearbyPokemon[0].DistanceMeters.toString() + ' meters')
+                    }
+                }
+
+            });
+        }, 5000);
+
+    });
+});
+
+b.init(username1, password1, location1, provider1, function(err) {
+    if (err) throw err;
+
+    console.log('[i] Current location: ' + b.playerInfo.locationName);
+    console.log('[i] lat/long/alt: : ' + b.playerInfo.latitude + ' ' + b.playerInfo.longitude + ' ' + b.playerInfo.altitude);
+
+    b.GetProfile(function(err, profile) {
         if (err) throw err;
 
         console.log('[i] Username: ' + profile.username);
@@ -33,21 +87,22 @@ Pokeio.init(username, password, location, provider, function(err) {
         console.log('[i] Pokecoin: ' + poke);
         console.log('[i] Stardust: ' + profile.currency[1].amount);
 
-
-        Pokeio.Heartbeat(function(err,hb) {
-            if(err) {
-                console.log(err);
-            }
-
-            for (var i = hb.cells.length - 1; i >= 0; i--) {
-                if(hb.cells[i].NearbyPokemon[0]) {
-                    //console.log(Pokeio.pokemonlist[0])
-                    var pokemon = Pokeio.pokemonlist[parseInt(hb.cells[i].NearbyPokemon[0].PokedexNumber)-1]
-                    console.log('[+] There is a ' + pokemon.name + ' at ' + hb.cells[i].NearbyPokemon[0].DistanceMeters.toString() + ' meters')
+        setInterval(function(){
+            b.Heartbeat(function(err,hb) {
+                if(err) {
+                    console.log(err);
                 }
-            }
 
-        });
+                for (var i = hb.cells.length - 1; i >= 0; i--) {
+                    if(hb.cells[i].NearbyPokemon[0]) {
+                        //console.log(a.pokemonlist[0])
+                        var pokemon = b.pokemonlist[parseInt(hb.cells[i].NearbyPokemon[0].PokedexNumber)-1]
+                        console.log('[+] There is a ' + pokemon.name + ' at ' + hb.cells[i].NearbyPokemon[0].DistanceMeters.toString() + ' meters')
+                    }
+                }
+
+            });
+        }, 5000);
 
     });
 });

378b896d61291e4081b548a7fd232c1927865d0c
Damian Trojnar <trojnar.damian@gmail.com>
2016-07-23 20:01:52 +0200

    Add single example to main.js

---
+++ botTick.js
@@ -0,0 +1,38 @@
+var _ = require("lodash")
+
+module.exports = function(pokeio){
+  var logNearbyPokemon = function(nearbyPokemon){
+    var pokemon = pokeio.pokemonlist[parseInt(nearbyPokemon.PokedexNumber) - 1]
+    console.log(
+      '[+] There is a ' + pokemon.name + ' at ' + nearbyPokemon.DistanceMeters + ' meters'
+    )
+  }
+
+  var catchWildPokemons = function(cell){
+    _.each(cell.WildPokemon, function(wildPokemon){
+      var pokedexInfo = pokeio.pokemonlist[parseInt(wildPokemon.pokemon.PokemonId)-1]
+      console.log('[+] There is a ' + pokedexInfo.name + ' near!! I can try to catch it!');
+
+      pokeio.EncounterPokemon(wildPokemon, function(suc, dat) {
+        console.log('Encountering pokemon ' + pokedexInfo.name + '...');
+        pokeio.CatchPokemon(wildPokemon, 1, 1.950, 1, 1, function(xsuc, xdat) {
+          // var status = ['Unexpected error', 'Successful catch', 'Catch Escape', 'Catch Flee', 'Missed Catch'];
+          // console.log(status[xdat.Status]);
+        });
+      });
+    })
+  }
+
+  var botTick = function (err, hb) {
+    if(err) { console.log("Error on botTick: ", err); }
+
+    _.each(hb.cells, function(cell){
+      nearbyPokemon = cell.NearbyPokemon[0];
+      if(nearbyPokemon) { logNearbyPokemon(nearbyPokemon) }
+
+      catchWildPokemons(cell);
+    }) // end enumerating cells
+  }
+
+  return botTick;
+}
---
+++ main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var PokemonGO = require('./poke.io.js');
+var botTick = require("./botTick.js")
+
+// using var so you can login with multiple users
+var pokeio = new PokemonGO.Pokeio()
+
+//Set environment variables or replace placeholder text
+var location = {
+  type: 'coords',
+  coords: {
+    latitude: parseFloat(process.env.PGO_LATITUDE),
+    longitude: parseFloat(process.env.PGO_LONGITUDE),
+    altitude: parseFloat(process.env.PGO_ALTITUDE),
+  }
+};
+
+var username = process.env.PGO_USERNAME || 'USER';
+var password = process.env.PGO_PASSWORD || 'PASS';
+var provider = process.env.PGO_PROVIDER || 'google';
+
+pokeio.init(username, password, location, provider, function(err) {
+    if (err) throw err;
+    var playerInfo = pokeio.playerInfo;
+    console.log('[i] Current location: ' + playerInfo.locationName);
+    console.log(
+      '[i] lat/long/alt: : ' +
+      playerInfo.latitude + ' ' + playerInfo.longitude + ' ' + playerInfo.altitude
+    );
+
+    pokeio.GetProfile(function(err, profile) {
+        if (err) throw err;
+
+        console.log('[i] Username: ' + profile.username);
+        console.log('[i] Poke Storage: ' + profile.poke_storage);
+        console.log('[i] Item Storage: ' + profile.item_storage);
+
+        var poke = 0;
+        if (profile.currency[0].amount) {
+            poke = profile.currency[0].amount;
+        }
+
+        console.log('[i] Pokecoin: ' + poke);
+        console.log('[i] Stardust: ' + profile.currency[1].amount);
+
+        setInterval(function(){ pokeio.Heartbeat(botTick(pokeio)); }, 5000);
+
+    });
+});

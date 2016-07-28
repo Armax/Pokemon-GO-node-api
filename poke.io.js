227175b1267958cbe5942bcbc7ecdaf8a959870e
Patrik Hartwig <patrikhartwig@192-168-1-13.tpgi.com.au>
2016-07-28 20:17:53 +1000

    UseItemEggIncubator, GetHatchedEggs added

--- poke.io.js
+++ poke.io.js
@@ -559,6 +559,62 @@ function Pokeio() {
 
   };
 
+  self.UseItemEggIncubator = function (item_id, pokemonId, callback) {
+
+    var levelUpRewards = new RequestEnvelop.UseItemEggIncubatorMessage({
+      'item_id': item_id,
+      'pokemonId': pokemonId
+    });
+    var req = new RequestEnvelop.Requests(140, levelUpRewards.encode().toBuffer());
+
+    var _self$playerInfo3 = self.playerInfo;
+    var apiEndpoint = _self$playerInfo3.apiEndpoint;
+    var accessToken = _self$playerInfo3.accessToken;
+
+    api_req(apiEndpoint, accessToken, req, function (err, f_ret) {
+      if (err) {
+        return callback(err);
+      } else if (!f_ret || !f_ret.payload || !f_ret.payload[0]) {
+        return callback('No result');
+      }
+
+      var dErr, response;
+      try {
+        response = ResponseEnvelop.UseItemEggIncubatorResponse.decode(f_ret.payload[0]);
+      } catch (err) {
+        dErr = err;
+      }
+      callback(err, response);
+    });
+
+  };
+
+
+  self.GetHatchedEggs = function (callback) {
+
+    var req = new RequestEnvelop.Requests(140);
+
+    var _self$playerInfo3 = self.playerInfo;
+    var apiEndpoint = _self$playerInfo3.apiEndpoint;
+    var accessToken = _self$playerInfo3.accessToken;
+
+    api_req(apiEndpoint, accessToken, req, function (err, f_ret) {
+      if (err) {
+        return callback(err);
+      } else if (!f_ret || !f_ret.payload || !f_ret.payload[0]) {
+        return callback('No result');
+      }
+
+      var dErr, response;
+      try {
+        response = ResponseEnvelop.GetHatchedEggsResponse.decode(f_ret.payload[0]);
+      } catch (err) {
+        dErr = err;
+      }
+      callback(err, response);
+    });
+
+  };
 
   self.GetLocationCoords = function () {
     var _self$playerInfo5 = self.playerInfo;
